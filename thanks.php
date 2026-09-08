36a909d4ef162625f9615839fef8c99e76b85131
Karim Ziouche <karimziouche.pro@outlook.fr>
2026-09-08 14:01:28 +0200

    Challenge 8: simuler une prise de contact

---
+++ thanks.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Merci" . " " . $_POST["user_first_name"] . " " . $_POST["user_name"] . " " . "de nous avoir contacté à propos de" ." ". $_POST["user_subject"];
+
+echo ".";
+
+echo " ". "Un de nos conseiller vous contactera soit à l’adresse" ." ". $_POST["user_email"] ." ". "ou par téléphone au" ." ".$_POST["user_number"] ." ". "dans les plus brefs délais pour traiter votre demande : " ."". $_POST["user_message"];
