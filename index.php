fb6216a426680bf51d5090e9c54fe976c43965e1
Karim Ziouche <karimziouche.pro@outlook.fr>
2026-09-08 11:04:40 +0200

    Challenge 7: Montre nous ton plus beau débug

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Karim Ziouche" => [
+        "city" => "Paris",
+        "weapon" => "gun"
+    ]
+];
+
+var_dump($characters);
