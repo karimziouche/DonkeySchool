3fe9839eb37936e62ee294ff1e298abdbc695ece
Karim Ziouche <karimziouche.pro@outlook.fr>
2026-09-11 15:49:33 +0200

    chalenge 11 : fais toi des amis

--- index.php
+++ index.php
@@ -1,22 +1,48 @@
-<?php
+<?php 
+require_once '_connec.php';
 
-$characters = [
-    "Negan" => [
-        "city" => "The Sanctuary",
-        "weapon" => "Lucille"
-    ],
-    "Daryl" => [
-        "city" => "Alexandria",
-        "weapon" => "crossbow"
-    ],
-    "Ezekiel" => [
-        "city" => "The Kingdom",
-        "weapon" => "Shiva"
-    ],
-    "Karim Ziouche" => [
-        "city" => "Paris",
-        "weapon" => "gun"
-    ]
-];
+$pdo = new \PDO(DSN, USER, PASS);
 
-var_dump($characters);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+
+    $query = "INSERT INTO friend (firstname, lastname)
+              VALUES (:firstname, :lastname)";
+
+    $statement = $pdo->prepare($query);
+
+    $statement->execute ([
+        'firstname' => $firstname,
+        'lastname' => $lastname
+    ]);
+}
+
+$query = "SELECT * FROM friend";
+$statement = $pdo->query($query);
+$friends = $statement->fetchAll();
+?>
+
+<html>
+<body>
+
+<h1>Mes amis</h1>
+<ul>
+    <?php foreach ($friends as $friend): ?>
+        <li>
+            <?= $friend['firstname'] ?> <?=  $friend['lastname'] ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+    <form method="POST">
+        <label for="firstname">Prénom :</label>
+        <input type="text" id="firstname" name="firstname" required>
+
+        <label for="lastname">Nom : </label>
+        <input type="text" id="lastname" name="lastname" required>
+
+        <button type="submit">Ajouter</button>
+    </form>
+</body>
+</html>
